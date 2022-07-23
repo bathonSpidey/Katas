82baf29719688ea71fb343761da63bb19428f443
Abir Bhattacharyya <46240907+bathonSpidey@users.noreply.github.com>
2022-07-23 19:57:33 +0200

    Updates

--- Katas.Tests/LiftTests.cs
+++ Katas.Tests/LiftTests.cs
@@ -33,4 +33,36 @@ public class LiftTests
 		};
 		Assert.That(Lift.TheLift(queues, 5), Is.EqualTo(new[] { 0, 2, 1, 0 }));
 	}
+
+	[Test]
+	public void SimpleUpAndUp()
+	{
+		int[][] queues =
+		{
+			new int[0], // G
+			new int[]{3}, // 1
+			new int[]{4}, // 2
+			new int[0], // 3
+			new int[]{5}, // 4
+			new int[0], // 5
+			new int[0],
+		};
+		Assert.That(Lift.TheLift(queues, 5), Is.EqualTo(new[] { 0, 1, 2, 3, 4, 5, 0 }));
+	}
+
+	[Test]
+	public void SimpleDownAndDown()
+	{
+		int[][] queues =
+		{
+			new int[0], // G
+			new int[]{0}, // 1
+			new int[0], // 2
+			new int[0], // 3
+			new int[]{2}, // 4
+			new int[]{3}, // 5
+			new int[0],
+		};
+		Assert.That(Lift.TheLift(queues, 5), Is.EqualTo(new[] { 0, 5, 4, 3, 2, 1, 0 }));
+	}
 }
--- Katas/Lift.cs
+++ Katas/Lift.cs
@@ -1,92 +1,102 @@
-﻿namespace Katas.Tests;
+﻿namespace Katas;
 
 public class Lift
 {
 	private static int totalCapacity;
-	private int direction;
+	private static List<int> stops = new();
+	private static int direction = 1;
 	private readonly List<int> spaceLeft;
-	private int currentFloor;
 
 	private Lift(int capacity)
 	{
 		totalCapacity = capacity;
 		spaceLeft = new List<int>();
-		currentFloor = 0;
-		direction = 1;
 	}
 
 	public static int[] TheLift(int[][] queues, int capacity)
 	{
 		var lift = new Lift(capacity);
-		var stops = new List<int> { 0 };
-		var personToAdd = new List<int>();
-		var count = 0;
+		stops = new List<int> { 0 };
+		direction = 1;
+		var notCleared = true;
+		while (notCleared)
+		{
+			LiftMovingUp(queues, lift);
+			LiftMovingDown(queues, lift);
+			if (!queues.Any(x => x.Length > 0))
+				notCleared = false;
+		}
+		if (stops[^1] != 0)
+			stops.Add(0);
+		return stops.ToArray();
+	}
+
+	private static void LiftMovingDown(int[][] queues, Lift lift)
+	{
+		for (var floor = queues.GetLength(0) - 1; floor >= 0; floor--)
+		{
+			if (queues[floor].Length == 0 && lift.spaceLeft.Count == 0)
+				continue;
+			GettingOff(lift, floor);
+			direction = -1;
+			LiftMoving(queues, lift, floor);
+		}
+	}
+
+	private static void LiftMovingUp(int[][] queues, Lift lift)
+	{
 		for (var floor = 0; floor < queues.GetLength(0); floor++)
 		{
-			lift.currentFloor = floor;
 			if (queues[floor].Length == 0 && lift.spaceLeft.Count == 0)
-			{
-				count++;
 				continue;
-			}
-			for (var person = 0; person < lift.spaceLeft.Count; person++)
-				if (lift.spaceLeft[person] == floor)
-				{
-					if (stops[^1] != floor)
-						stops.Add(floor);
-					lift.spaceLeft.RemoveAt(person);
-				}
-			if (lift.spaceLeft.Count != totalCapacity && queues[floor].Length != 0)
-			{
-				for (var person = 0; person < queues[floor].Length; person++)
-					if (queues[floor][person] > floor && lift.spaceLeft.Count != capacity)
-					{
-						if (stops[^1] != floor)
-							stops.Add(floor);
-						personToAdd.Add(queues[floor][person]);
-					}
-				foreach (var person in personToAdd)
-				{
-					var indexToRemove=Array.IndexOf(queues[floor], person);
-					queues[floor]= queues[floor].Where((source, index) => index != indexToRemove).ToArray();
-				}
-				lift.spaceLeft.AddRange(personToAdd);
-			}
+			GettingOff(lift, floor);
+			direction = 1;
+			LiftMoving(queues, lift, floor);
 		}
-		for (var floor = queues.GetLength(0)-1; floor >= 0; floor--)
-		{
-			lift.currentFloor = floor;
-			if (queues[floor].Length == 0 && lift.spaceLeft.Count == 0)
-			{
-				count++;
-				continue;
-			}
-			for (var person = 0; person < lift.spaceLeft.Count; person++)
-				if (lift.spaceLeft[person] == floor)
-				{
-					if (stops[^1] != floor)
-						stops.Add(floor);
-					lift.spaceLeft.RemoveAt(person);
-				}
-			if (lift.spaceLeft.Count != totalCapacity && queues[floor].Length != 0)
-			{
-				for (var person = 0; person < queues[floor].Length; person++)
-					if (queues[floor][person] < floor && lift.spaceLeft.Count != capacity)
-					{
-						if (stops[^1] != floor)
-							stops.Add(floor);
-						personToAdd.Add(queues[floor][person]);
-					}
-				foreach (var person in personToAdd)
-				{
-					var indexToRemove = Array.IndexOf(queues[floor], person);
-					queues[floor] = queues[floor].Where((source, index) => index != indexToRemove).ToArray();
-				}
-				lift.spaceLeft.AddRange(personToAdd);
-			}
+	}
 
+	private static void LiftMoving(int[][] queues, Lift lift, int floor)
+	{
+		if (lift.spaceLeft.Count != totalCapacity && queues[floor].Length != 0)
+		{
+			var personToAdd = new List<int>();
+			for (var person = 0; person < queues[floor].Length; person++)
+			{
+				if (queues[floor][person] > floor && lift.spaceLeft.Count != totalCapacity &&
+						direction == 1)
+				{
+					if (stops[^1] != floor)
+						stops.Add(floor);
+					personToAdd.Add(queues[floor][person]);
+				}
+				if (queues[floor][person] < floor && lift.spaceLeft.Count != totalCapacity &&
+						direction == -1)
+				{
+					if (stops[^1] != floor)
+						stops.Add(floor);
+					personToAdd.Add(queues[floor][person]);
+				}
+			}
+			GettingOn(queues, personToAdd, floor, lift);
 		}
-		stops.Add(0);
-		return stops.ToArray();
+	}
+
+	private static void GettingOn(int[][] queues, List<int> personToAdd, int floor, Lift lift)
+	{
+		foreach (var indexToRemove in personToAdd.Select(person =>
+							Array.IndexOf(queues[floor], person)))
+			queues[floor] = queues[floor].Where((_, index) => index != indexToRemove).ToArray();
+		lift.spaceLeft.AddRange(personToAdd);
+	}
+
+	private static void GettingOff(Lift lift, int floor)
+	{
+		for (var person = 0; person < lift.spaceLeft.Count; person++)
+			if (lift.spaceLeft[person] == floor)
+			{
+				if (stops[^1] != floor)
+					stops.Add(floor);
+				lift.spaceLeft.RemoveAt(person);
+			}
 	}
 }
